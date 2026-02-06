ea937cb68be61f6f646ab05403b2359e51513e77
Shubhangi Pattewar <shubhangipattewar5@gmail.com>
2026-02-06 12:44:55 +0530

    Add files via upload

---
+++ MergeSortedArrays.java
@@ -0,0 +1,28 @@
+public class MergeSortedArrays {
+public static void main(String[] args) {
+int[] a = {1, 3, 5, 7, 9};
+int[] b = {2, 4, 6, 8, 10};
+int[] c = new int[a.length + b.length];
+int i = 0, j = 0, k = 0;
+
+while (i < a.length && j < b.length){
+if (a[i] < b[j]) {
+	c[k++] = a[i++];
+} else {
+	c[k++] = b[j++];
+}
+}
+
+while (i < a.length){
+	c[k++] = a[i++];
+}
+while (j < b.length){
+	c[k++] = b[j++];
+}
+
+System.out.println("Merged Sorted Array : ");
+for (int x : c){
+	System.out.println(x + " ");
+}
+}
+}
